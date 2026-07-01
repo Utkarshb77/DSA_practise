762b84feb67578eced84645aea23a3b644ade29a
Utkarsh Baghel <utkarshbaghel18@gmail.com>
2026-07-01 23:18:41 +0530

    Time: 0 ms (100%), Space: 41.7 MB (96.82%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -1,13 +1,7 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
         if( num < 10) return true;
-        // int last = 0;
-        // int n = num;
-        // while(n > 0){
-        //     last = n % 10;
-        //     n = n/10;
-        // }
-        if(num % 10 == 0) return false;
-        return true;
+        // if(num % 10 == 0) return false
+        return num % 10 != 0;
     }
 }
