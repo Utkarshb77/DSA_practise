f5bbd7be68695ff56e23c47ae8257b09442ce12b
Utkarsh Baghel <utkarshbaghel18@gmail.com>
2026-08-10 13:21:39 +0530

    Time: 5 ms (97.59%), Space: 44.1 MB (60.69%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,28 +1,22 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if( s1.length() > s2.length()) return false;
-        int[] freq = new int[26];
-        for(int i=0;i<s1.length();i++){
-            freq[ s1.charAt(i) - 'a']++;
-        }
+        if(s1.length() > s2.length()) return false;
+        int[] arr = new int[26];
+        for(int i=0;i<s1.length();i++) arr[ s1.charAt(i) - 'a']++ ;
         int windowsize = s1.length();
-        for(int i=0;i<windowsize;i++){
-            freq[ s2.charAt(i) - 'a']--;
-        }
-        if( check(freq)) return true;
+        for(int i=0;i<windowsize;i++) arr[s2.charAt(i)-'a']--;
+        if(allzero(arr)) return true;
         for(int i=windowsize;i<s2.length();i++){
-            char c = s2.charAt(i);
-            char del = s2.charAt(i-windowsize);
-            freq[c-'a']--;
-            freq[del-'a']++;
-            if(check(freq)) return true;
+            int idx1 =s2.charAt(i)-'a';
+            int idx2 = s2.charAt(i-windowsize)  - 'a';
+            arr[idx1]--;
+            arr[idx2]++;
+            if(allzero(arr)) return true;
         }
         return false;
     }
-    public static boolean check(int[] freq){
-        for(int x : freq){
-            if( x != 0) return false;
-        }
+    public static boolean allzero(int[] arr){
+        for(int i : arr) if(i != 0) return false;
         return true;
     }
 }
